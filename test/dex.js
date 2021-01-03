15840b1df385aede6fca630811f46fc4ff984891
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-03 16:36:21 -0700

    Import mock ERC20 tokens

---
+++ test/dex.js
@@ -0,0 +1,4 @@
+const Dai = artifacts.require('mocks/Dai.sol');
+const Bat = artifacts.require('mocks/Bat.sol');
+const Rep = artifacts.require('mocks/Rep.sol');
+const Zrx = artifacts.require('mocks/Zrx.sol');
