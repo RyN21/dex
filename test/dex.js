38baeaa527dc1f1eb9e024cbb1e9b56920607cac
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-03 16:39:55 -0700

    Define ERC20 toekn vairables before each test

--- test/dex.js
+++ test/dex.js
@@ -4,5 +4,14 @@ const Rep = artifacts.require('mocks/Rep.sol');
 const Zrx = artifacts.require('mocks/Zrx.sol');
 
 contract('Dex', () => {
+  let dai, bat, rep, zrx;
 
+  beforeEach(async() => {
+    ([dai, bat, rep, zrx] = await Promise.all([
+      Dai.new(),
+      Bat.new(),
+      Rep.new(),
+      Zrx.new()
+    ]));
+  });
 });
