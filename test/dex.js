dd708df3a6b11854793a6e89afcabad6b952b4e8
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-03 16:37:04 -0700

    Define test contract

--- test/dex.js
+++ test/dex.js
@@ -2,3 +2,7 @@ const Dai = artifacts.require('mocks/Dai.sol');
 const Bat = artifacts.require('mocks/Bat.sol');
 const Rep = artifacts.require('mocks/Rep.sol');
 const Zrx = artifacts.require('mocks/Zrx.sol');
+
+contract('Dex', () => {
+
+});
