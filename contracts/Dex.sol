b092fa37575ee6eed29626c68511714c4048f437
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-03 17:28:39 -0700

    Configure ERC20 tokens into the smart contract

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.6.3;
 
 contract Dex {
-  
 }
--- test/dex.js
+++ test/dex.js
@@ -7,6 +7,10 @@ const Dex = artifacts.require('Dex.sol');
 contract('Dex', () => {
   let dai, bat, rep, zrx;
 
+  // produce tickers for through Ascii
+  const [DAI, BAT, REP, ZRX] = ['DAI', 'BAT', 'REP', 'ZRX']
+    .map(ticker => web3.utils.fromAscii(ticker));
+
   beforeEach(async() => {
     ([dai, bat, rep, zrx] = await Promise.all([
       Dai.new(),
@@ -15,5 +19,11 @@ contract('Dex', () => {
       Zrx.new()
     ]));
     const dex = await Dex.new();
+    await Promise.all([
+      dex.addToken(DAI ,dai.address),
+      dex.addToken(BAT ,bat.address),
+      dex.addToken(REP ,rep.address),
+      dex.addToken(ZRX ,zrx.address)
+    ]);
   });
 });
