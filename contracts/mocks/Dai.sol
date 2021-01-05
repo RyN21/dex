d85ca32d921e30122ef1e377899158e156cd4527
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-04 21:53:33 -0700

    Add faucet functions to mock tokens

--- contracts/mocks/Dai.sol
+++ contracts/mocks/Dai.sol
@@ -5,4 +5,8 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 // Dia smart inherits from ERC20
 contract Dai is ERC20 {
   constructor() ERC20('Dai Stablecoin', 'Dai') public {}
+
+  function faucet(address to, uint amount) external {
+    _mint(to, amount);
+  }
 }
