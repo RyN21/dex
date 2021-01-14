f982235e6edc5ff03006ba365604133416acc5a9
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-14 08:36:26 -0700

    Create trades

--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -84,4 +84,32 @@ module.exports = async function(deployer, _network, accounts) {
       id: 0,
     }, () => {});
   }
+
+  //create trades
+  await dex.createLimitOrder(BAT, 1000, 10, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(BAT, 1000, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(BAT, 1200, 11, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(BAT, 1200, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(BAT, 1200, 15, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(BAT, 1200, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(BAT, 1500, 14, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(BAT, 1500, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(BAT, 2000, 12, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(BAT, 2000, SIDE.SELL, {from: trader2});
+  await dex.createLimitOrder(REP, 1000, 2, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(REP, 1000, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(REP, 500, 4, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(REP, 500, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(REP, 800, 2, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(REP, 800, SIDE.SELL, {from: trader2});
+  await increaseTime(1);
+  await dex.createLimitOrder(REP, 1200, 6, SIDE.BUY, {from: trader1});
+  await dex.createMarketOrder(REP, 1200, SIDE.SELL, {from: trader2});
+
 };
