5c37598a5a527105c7a734aad4e2a3ef20dd7271
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-15 16:10:14 -0700

    Update createMarketOrder function with safeMath

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -183,10 +183,10 @@ contract Dex {
     uint remaining = amount;
 
     while(i < orders.length && remaining > 0) {
-      uint available = orders[i].amount - orders[i].filled;
+      uint available = orders[i].amount.sub(orders[i].filled);
       uint matched = (remaining > available) ? available : remaining;
-      remaining -= matched;
-      orders[i].filled += matched;
+      remaining = remaining.sub(matched);
+      orders[i].filled = orders[i].filled.add(matched);
       emit NewTrade(
         nextTradeId,
         orders[i].id,
@@ -198,20 +198,20 @@ contract Dex {
         now
       );
       if(side == Side.SELL) {
-        traderBalances[msg.sender][ticker] -= matched;
-        traderBalances[msg.sender][DAI] += matched * orders[i].price;
-        traderBalances[orders[i].trader][ticker] += matched;
-        traderBalances[orders[i].trader][DAI] -= matched * orders[i].price;
+        traderBalances[msg.sender][ticker] = traderBalances[msg.sender][ticker].sub(matched);
+        traderBalances[msg.sender][DAI] = traderBalances[msg.sender][DAI].add(matched.mul(orders[i].price));
+        traderBalances[orders[i].trader][ticker] = traderBalances[orders[i].trader][ticker].add(matched);
+        traderBalances[orders[i].trader][DAI] = traderBalances[orders[i].trader][DAI].sub(matched.mul(orders[i].price));
       }
       if(side == Side.BUY) {
         require(
           traderBalances[msg.sender][DAI] >= matched * orders[i].price,
           'DAI balance is too low.'
         );
-        traderBalances[msg.sender][ticker] += matched;
-        traderBalances[msg.sender][DAI] -= matched * orders[i].price;
-        traderBalances[orders[i].trader][ticker] -= matched;
-        traderBalances[orders[i].trader][DAI] += matched * orders[i].price;
+        traderBalances[msg.sender][ticker] = traderBalances[msg.sender][ticker].add(matched);
+        traderBalances[msg.sender][DAI] = traderBalances[msg.sender][DAI].sub(matched.mul(orders[i].price));
+        traderBalances[orders[i].trader][ticker] = traderBalances[orders[i].trader][ticker].sub(matched);
+        traderBalances[orders[i].trader][DAI] += traderBalances[orders[i].trader][DAI].sub(matched.mul(orders[i].price));
       }
       nextTradeId++;
       i++;
