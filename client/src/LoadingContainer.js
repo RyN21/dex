b9c05227cb62866a76e53d2216a6cf176ea40ed6
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-25 20:37:24 -0700

    Setup state in init function

--- client/src/LoadingContainer.js
+++ client/src/LoadingContainer.js
@@ -6,6 +6,18 @@ function LoadingContainer() {
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState([]);
   const [contracts, setContracts] = useState(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.getAccounts();
+      const contracts = await getContracts(web3);
+      setWeb3(web3);
+      setAccounts(accounts);
+      setContracts(contracts);
+    }
+    init();
+  }, []);
 }
 
 export default LoadingContainer;
