888bd2cbf579f30779b9b4718c2f1b0c277afd2e
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-25 20:31:01 -0700

    Define some state

--- client/src/LoadingContainer.js
+++ client/src/LoadingContainer.js
@@ -3,7 +3,9 @@ import { getWeb3, getContracts } from './utils.js';
 import App from './App.js';
 
 function LoadingContainer() {
-
+  const [web3, setWeb3] = useState(undefined);
+  const [accounts, setAccounts] = useState([]);
+  const [contracts, setContracts] = useState(undefined);
 }
 
 export default LoadingContainer;
